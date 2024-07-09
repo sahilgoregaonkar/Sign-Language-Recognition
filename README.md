45015cb3321c003351cfffb57c2a78d577680119
sahilgoregaonkar <sahilgoregaonkar123@gmail.com>
2024-07-09 23:12:28 +0530

    data collection added

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Sign-Language-Recognition
+
+
+## How to run:
+
+```bash
+
+conda create -n signLang python=3.7 -y
+
+```
+
+```bash
+
+conda activate signLang
+
+```
+
+```bash
+
+pip install -r requirements.txt
+
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="signLanguages",
+    version = "0.0.0",
+    REPO_NAME = "Sign-Language-Recognition",
+    AUTHOR_USER_NAME = "sahilgoregaonkar",
+    AUTHOR_EMAIL = "sahilgoregaonkar123@gmail.com",
+    packages=find_packages(),
+    install_requires = [],
+)
+
+
